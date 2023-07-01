1fac713ccc814dbf8d4d70d12cd112f40a10cf7b
shubhanker saxena <ankursaxena347@gmail.com>
2023-07-01 15:34:24 +0530

    repo added

---
+++ app/component/page.tsx
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Whyus from "./Whysus";
+import Process from "./Process";
+import Jobs from "./Jobs";
+import Fotter from "./Fotter";
+const dashboard = () => {
+    return (
+    <>
+    <Navbar />
+    <Hero />
+    <Whyus />
+    <Process />
+    <Jobs />
+    <Fotter />
+    </>
+    )
+}
+
+export default dashboard;
---
+++ app/secondPage/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Fotter from '../component/Fotter';
+import Navbar from '../component/Navbar';
+
+function page() {
+  return (
+    <div>
+        <Navbar />
+    
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default page;
